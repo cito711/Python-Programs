ec5989f280e278fcf12af7f66ab7120115ba0a7f
cito711 <garciape711github@hotmail.com>
2018-01-15 15:07:56 -0600

    Add files via upload

---
+++ helloPython.py
@@ -0,0 +1,28 @@
+import random
+import sys
+import os
+
+print("Hello World")
+
+# Comment
+
+'''This is
+a comment '''
+
+name = "Pedro"
+print(name)
+name = 15
+
+print("5 + 2 =", 5+2)
+print("7 * 2 =", 7 * 2)
+print("4 ** 5 =", 4 ** 5)
+
+quote = "\"Always Remember you are unique"
+
+multi_line_quote = '''just 
+like everyone else'''
+
+print("%s %s %s" % ('I like the quote', quote, multi_line_quote))
+
+print("I don't like ", end="")
+print("newlines")
