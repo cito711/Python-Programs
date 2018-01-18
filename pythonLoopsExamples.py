d11d0ac8b930b0017e8ef1fa1069ef5090e6a142
cito711 <garciape711github@hotmail.com>
2018-01-18 15:36:45 -0600

    Add files via upload

---
+++ pythonLoopsExamples.py
@@ -0,0 +1,42 @@
+import random
+import os
+import sys
+
+for x in range(0, 10):
+    print(x, ' ', end="")
+
+print('\n')
+
+grocery_list = ['Juice', 'Tomatoes', 'Potatoes', 'Bananas']
+
+for y in grocery_list:
+    print(y)
+
+for x in [2,4,6,8,10]:
+    print(x)
+
+num_list = [[1,2,3],[10,20,30],[100,200,300]]
+
+for x in range(0,3):
+    for y in range(0,3):
+        print(num_list[x][y])
+print('\n')
+
+random_num = random.randrange(0,100)
+
+while(random_num != 15):
+    print(random_num)
+    random_num = random.randrange(0,100)
+
+
+i = 0;
+
+while(i <= 20):
+    if(i%2 == 0):   # used to find even numbers
+        print(i)
+    elif(i == 9):
+        break
+    else:
+        i += 1      # i = i +1
+        continue
+    i += 1
